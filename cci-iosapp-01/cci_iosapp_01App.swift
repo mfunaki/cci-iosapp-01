f9659df475ae9fe7ab1333ea0d2c8930ef14bc01
Masahiko Funaki <mfunaki@mayoct.net>
2023-01-12 09:20:13 +0800

    first commit

---
+++ cci-iosapp-01/cci_iosapp_01App.swift
@@ -0,0 +1,17 @@
+//
+//  cci_iosapp_01App.swift
+//  cci-iosapp-01
+//
+//  Created by Masahiko Funaki on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct cci_iosapp_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
